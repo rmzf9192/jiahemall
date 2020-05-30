da0fbc504de60057517e8b8e48fd43140a4d1d5b
fei.zhang <fei.zhang@elitesland.com>
2020-05-30 19:00:00 +0800

    检索服务启动类

---
+++ jiahemall-search/src/main/java/com/el/jiahemall/search/JiahemallSearchApplication.java
@@ -0,0 +1,16 @@
+package com.el.jiahemall.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class JiahemallSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JiahemallSearchApplication.class, args);
+    }
+
+}
